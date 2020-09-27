12f84ba7aa8830929c539b61be279235cb149dd8
Robin <dtr.robin09@gmail.com>
2020-09-27 23:11:28 +0200

    added images from git and step 3 done

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+
+    private fun initViews() {
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        binding.rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
     }
 }
