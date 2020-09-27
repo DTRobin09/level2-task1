126b4e86b6ede30c0158af4a703bb7ca86a796c7
Robin <dtr.robin09@gmail.com>
2020-09-27 22:59:30 +0200

    variables step 3

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
